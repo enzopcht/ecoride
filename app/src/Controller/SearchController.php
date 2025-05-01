f31c3272811ca5454f0c539b3998e6c56dbec50f
Enzo Pauchet <enzo.pauchet@gmail.com>
2025-05-02 00:11:23 +0200

    feat(search): Setting up the controller, form and views

---
+++ app/src/Controller/SearchController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class SearchController extends AbstractController
+{
+    #[Route('/search', name: 'app_search')]
+    public function index(): Response
+    {
+        return $this->render('search/index.html.twig', [
+            'controller_name' => 'SearchController',
+        ]);
+    }
+
+    #[Route('/search/results', name: 'app_search_results')]
+    public function results(): Response
+    {
+        return $this->render('search/results.html.twig');
+    }
+}
