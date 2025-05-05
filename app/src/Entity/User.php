9ce1e40160ba0d73293b7392f341717ed557f20a
Enzo Pauchet <enzo.pauchet@gmail.com>
2025-05-06 00:46:14 +0200

    feat(submit-form) : establishment of conditions and security for the submission of the form

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -11,6 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 #[UniqueEntity(fields: ['pseudo'], message: 'Ce pseudo est déjà utilisé.')]
+#[UniqueEntity(fields: ['email'], message: 'Cet email est déjà utilisé.')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
