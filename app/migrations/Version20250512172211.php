95717a372f58a75ca42201d7af54917c8b2a69e6
Enzo Pauchet <enzo.pauchet@gmail.com>
2025-05-12 23:32:06 +0200

    feat(add-ride): Integration of the page to add a new trip. Implementation of the form, controller, backend and frontend security.

---
+++ app/migrations/Version20250512172211.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250512172211 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE ride ADD departure_post_code VARCHAR(10) NOT NULL, ADD arrival_post_code VARCHAR(10) NOT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE ride DROP departure_post_code, DROP arrival_post_code
+        SQL);
+    }
+}
