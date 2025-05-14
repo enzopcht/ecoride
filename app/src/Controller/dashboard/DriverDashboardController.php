249de307c2f88dd1f877505ca4d4757a2e261413
Enzo Pauchet <enzo.pauchet@gmail.com>
2025-05-14 13:07:02 +0200

    feat(add-custom-preferences):Added functionality to add custom preference

--- app/src/Controller/dashboard/DriverDashboardController.php
+++ app/src/Controller/dashboard/DriverDashboardController.php
@@ -2,14 +2,11 @@
 
 namespace App\Controller\dashboard;
 
-use App\Document\DriverPreference;
 use App\Repository\CreditTransactionRepository;
 use App\Repository\ParticipationRepository;
 use App\Repository\ReviewRepository;
 use App\Repository\RideRepository;
-use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -68,47 +65,6 @@ final class DriverDashboardController extends AbstractController
             'rides' => $rides,
             'participations' => $participations,
         ]);
-    }
-    #[Route('/driver/dashboard/preferences', name: 'app_driver_preferences')]
-    public function preferences(
-        DocumentManager $dm,
-    ): Response {
-        $user = $this->getUser();
-        /** @var \App\Entity\User $user */
-        $preferences = $dm->getRepository(DriverPreference::class)->findOneBy(['userId' => $user->getId()]);
-        return $this->render('dashboard/driver/preferences.html.twig', [
-            'preferences' => $preferences,
-        ]);
-    }
-    #[Route('/driver/dashboard/preferences/update', name: 'app_driver_preferences_update', methods: ['POST'])]
-    #[IsGranted('ROLE_DRIVER')]
-    public function updatePreferences(
-        Request $request,
-        DocumentManager $dm
-    ): Response {
-        $user = $this->getUser();
-
-        if (!$this->isCsrfTokenValid('update_preferences', $request->request->get('_token'))) {
-            throw $this->createAccessDeniedException('Jeton CSRF invalide');
-        }
-
-        /** @var \App\Entity\User $user */
-        $preference = $dm->getRepository(\App\Document\DriverPreference::class)->findOneBy(['userId' => $user->getId()]);
-        if (!$preference) {
-            $preference = new \App\Document\DriverPreference();
-            $preference->setUserId($user->getId());
-        }
-
-        $preference->setMusicAllowed($request->request->has('musicAllowed'));
-        $preference->setSmokingAllowed($request->request->has('smokingAllowed'));
-        $preference->setAnimalsAllowed($request->request->has('animalsAllowed'));
-
-        $dm->persist($preference);
-        $dm->flush();
-
-        $this->addFlash('success', 'Préférences mises à jour avec succès.');
-
-        return $this->redirectToRoute('app_driver_preferences');
     }
     #[Route('/driver/dashboard/your-rides', name: 'app_driver_your_rides')]
     public function yourRides(
