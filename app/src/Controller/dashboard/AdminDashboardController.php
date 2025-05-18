fbdc062a254e7e2fea929551460d0d3408c0bafd
Enzo Pauchet <enzo.pauchet@gmail.com>
2025-05-18 16:09:57 +0200

    feat(create-employee): Creation of the administrator space and implementation of a functionality to create an employee.

--- app/src/Controller/dashboard/AdminDashboardController.php
+++ app/src/Controller/dashboard/AdminDashboardController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller\dashboard;
 
+use App\Entity\CreditTransaction;
+use App\Entity\User;
+use App\Form\EmployeType;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -13,8 +20,39 @@ final class AdminDashboardController extends AbstractController
     #[IsGranted('ROLE_ADMIN')]
     public function admin(): Response
     {
-        return $this->render('dashboard/admin.html.twig', [
+        return $this->render('dashboard/admin/admin.html.twig', [
             'user' => $this->getUser(),
+        ]);
+    }
+
+    #[Route('/admin/dashboard/create-employes', name: 'app_create_employes')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function createEmployes(
+        Request $request,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('plainPassword')->getData();
+            $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+            $user->setPassword($hashedPassword);
+            $user->setRoles(['ROLE_EMPLOYE']);
+
+            $em->persist($user);
+
+            $em->flush();
+
+            $this->addFlash('primary', 'Le compte employé a bien été créé.');
+            return $this->redirect($request->headers->get('referer'));
+        }
+
+        return $this->render('dashboard/admin/create-employes.html.twig', [
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
