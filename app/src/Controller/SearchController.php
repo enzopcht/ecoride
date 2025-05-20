c01f06dbcfa79986fc9c01922c33e786d282fd96
Enzo Pauchet <enzo.pauchet@gmail.com>
2025-05-20 11:56:53 +0200

    fix(display-driver-note): display a driver's rating on the results page

--- app/src/Controller/SearchController.php
+++ app/src/Controller/SearchController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\SearchRideType;
+use App\Repository\ReviewRepository;
 use App\Repository\RideRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,9 +25,9 @@ final class SearchController extends AbstractController
     #[Route('/search/results', name: 'app_search_results', methods: ['GET', 'POST'])]
     public function results(
         Request $request,
-        RideRepository $rideRepository
-        ): Response
-    {
+        RideRepository $rideRepository,
+        ReviewRepository $reviewRepository,
+    ): Response {
         $form = $this->createForm(SearchRideType::class);
         $form->handleRequest($request);
 
@@ -36,19 +37,34 @@ final class SearchController extends AbstractController
                 $this->addFlash('warning', 'Impossible de rechercher un trajet dans le passé.');
                 return $this->redirectToRoute('app_search');
             }
-        
+            $ratingsByDriver = [];
+
             $rides = $rideRepository->findRidesBySearchData(
                 $data['departure'],
                 $data['arrival'],
                 $data['date']
             );
 
+            foreach ($rides as $ride) {
+                $driver = $ride->getDriver();
+                $driverId = $driver->getId();
+                if (!isset($ratingsByDriver[$driverId])) {
+                    $ratingsByDriver[$driverId] = $reviewRepository->getAverageRatingForUser($driver);
+                }
+            }
+
             $alternateRide = $rideRepository->findNextRideAfterDate(
                 $data['departure'],
                 $data['arrival'],
                 $data['date']
             );
-
+            // foreach ($alternateRide as $ride) {
+            //     $driver = $ride->getDriver();
+            //     $driverId = $driver->getId();
+            //     if (!isset($ratingsByDriver[$driverId])) {
+            //         $ratingsByDriver[$driverId] = $reviewRepository->getAverageRatingForUser($driver);
+            //     }
+            // }
             $alternateForm = $this->createForm(SearchRideType::class, [
                 'departure' => $data['departure'],
                 'arrival' => $data['arrival'],
@@ -60,6 +76,7 @@ final class SearchController extends AbstractController
                 'data' => $data,
                 'rides' => $rides,
                 'alternateRide' => $alternateRide,
+                'ratings_by_driver' => $ratingsByDriver,
                 'alternateForm' => $alternateForm->createView(),
             ]);
         }
