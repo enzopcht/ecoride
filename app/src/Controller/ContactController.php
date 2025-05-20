403435a9d7b6d141a848a6dd835b830367ec474e
Enzo Pauchet <enzo.pauchet@gmail.com>
2025-05-20 17:35:34 +0200

    feat(contact-us): Setting up the contact us section

---
+++ app/src/Controller/ContactController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+final class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(
+        Request $request,
+        MailerInterface $mailer,
+    ): Response {
+        $form = $this->createForm(ContactType::class, null);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+
+            $email = (new Email())
+                ->from('support@ecoride.fr')
+                ->to('support@ecoride.fr')
+                ->subject('Nouveau message de contact')
+                ->text(
+                    "Nom : {$formData['first_name']} {$formData['last_name']}\n" .
+                    "Email : {$formData['email']}\n\n" .
+                    $formData['message']
+                );
+
+            $mailer->send($email);
+            $this->addFlash('success', 'Votre message a bien été envoyé.');
+        }
+        return $this->render('contact/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
